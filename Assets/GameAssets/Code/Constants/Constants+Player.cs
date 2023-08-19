2733a360f6cea884ad429306ef0d39c78b2e119b
Gaibryol <gaibryol@gmail.com>
2023-08-19 00:03:48 -0600

    Added player dash attack

--- Assets/GameAssets/Code/Constants/Constants+Player.cs
+++ Assets/GameAssets/Code/Constants/Constants+Player.cs
@@ -7,11 +7,14 @@ public partial class Constants
 			public const float Movespeed = 10f;
 			public const float DashForce = 25f;
 			public const float DashDuration = 0.15f;
+			public const float DashAttackForce = 25f;
+			public const float DashAttackDuration = 0.20f;
 		}
 		
 		public class Animations
 		{
 			public const string IsDashing = "IsDashing";
+			public const string IsDashAttacking = "IsDashAttacking";
 			public const string IsMoving = "IsMoving";
 		}
 	}
